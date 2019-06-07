6bfdd5de24f789d8a027c0f41f4222b37ead08dd
[none] <[nwariara@safaricom.co.ke]>
2019-06-07 10:38:40 +0100

    completed the pojo for department class.

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -2,6 +2,7 @@ package models;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Department {
     private String name;
@@ -17,17 +18,27 @@ public class Department {
         this.createdAt = LocalDateTime.now();
     }
 
-    public int getId() {return id;}
-    public LocalDateTime getCreatedAt() {return createdAt;}
+    public void setName(String name){this.name = name;}
     public String getName(){return name;}
+
+    public void setId (int id) {this.id = id;}
+    public int getId() {return id;}
+
+    public LocalDateTime getCreatedAt() {return createdAt;}
+
+
 //    public static ArrayList<Department> getAll(){return instances;}
 //    public static Department findById(int id){return instances.get(id-1);}
 ////    public void update(String content) {this.name = content;}
 //    public void deleteDepartment(){instances.remove(id-1);}
 //    public static void clearAllDeparments(){instances.clear();}
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
     }
+
+}
 
 
 
