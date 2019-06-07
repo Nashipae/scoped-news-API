2f0f6158a32ad298ce22e110dd14a11ea082eb85
[none] <[nwariara@safaricom.co.ke]>
2019-06-07 18:11:55 +0100

    added the get and post method in App java class.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import models.Department;
 import dao.Sql2oDepartmentDao;
 import java.util.List;
@@ -9,79 +10,44 @@ import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
+
     public static void main(String[] args) {
         staticFileLocation("/public");
         String connectionString = "jdbc:h2:~/todolist.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'";
         Sql2o sql2o = new Sql2o(connectionString, "", "");
         Sql2oDepartmentDao departmentDao = new Sql2oDepartmentDao(sql2o);
+        Gson gson = new Gson();
 
-        //get: delete all departments
-        get("/departments/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            departmentDao.clearAllDepartments();
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
-
-        //get: delete an individual department
-        get("/departments/:id/delete", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfDepartmentToDelete = Integer.parseInt(req.params("id"));
-            departmentDao.deleteById(idOfDepartmentToDelete);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
 
         //get: show all departments
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            List<Department> departments = departmentDao.getAll();
-            model.put("departments", departments);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+//        get("/", (req, res) -> {
+//            Map<String, Object> model = new HashMap<>();
+//            List<Department> departments = departmentDao.getAll();
+//            model.put("departments", departments);
+//            return new ModelAndView(model, "index.hbs");
+//        }, new HandlebarsTemplateEngine());
 
-        //get: show new department form
-        get("/departments/new", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return new ModelAndView(model, "department-form.hbs");
-        }, new HandlebarsTemplateEngine());
+        post("/departments/new", "application/json", (req, res) -> {//accept a request in format JSON from an app
+            Department department = gson.fromJson(req.body(), Department.class);//make java from JSON with GSON
+            departmentDao.add(department);
+            res.status(201);
+            res.type("application/json");
+            return gson.toJson(department);
+        });
 
-        //department: process new department form
-        post("/departments", (req, res) -> { //URL to make new department on POST route
-            Map<String, Object> model = new HashMap<>();
-            String description = req.queryParams("description");
-            Department newDepartment = new Department(description);
-            departmentDao.add(newDepartment);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
+        get("/departments", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            return gson.toJson(departmentDao.getAll());//send it back to be displayed
+        });
 
-        //get: show an individual department
-        get("/departments/:id", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfDepartmentToFind = Integer.parseInt(req.params("id"));
-            Department foundDepartment = departmentDao.findById(idOfDepartmentToFind);
-            model.put("department", foundDepartment);
-            return new ModelAndView(model, "department-detail.hbs");
-        }, new HandlebarsTemplateEngine());
+        get("/departments/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            int departmentId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(departmentDao.findById(departmentId));
+        });
+    }
 
-        //get: show a form to update a department
-        get("/departments/:id/update", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            int idOfDepartmentToEdit = Integer.parseInt(req.params("id"));
-            Department editDepartment = departmentDao.findById(idOfDepartmentToEdit);
-            model.put("editDepartment", editDepartment);
-            return new ModelAndView(model, "department-form.hbs");
-        }, new HandlebarsTemplateEngine());
 
-        //department: process a form to update a department
-        post("/departments/:id", (req, res) -> { //URL to update department on POST route
-            Map<String, Object> model = new HashMap<>();
-            String newContent = req.queryParams("description");
-            int idOfDepartmentToEdit = Integer.parseInt(req.params("id"));
-            departmentDao.update(idOfDepartmentToEdit, newContent);
-            res.redirect("/");
-            return null;
-        }, new HandlebarsTemplateEngine());
     }
 }
