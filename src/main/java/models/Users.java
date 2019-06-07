b7ccc4fce8d66540ef0f67ab1181815d2d5ca32d
[none] <[nwariara@safaricom.co.ke]>
2019-06-07 15:53:25 +0100

    added the usersDao interface.

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,21 @@
 package models;
 
 public class Users {
+    private String staffName;
+    private String staffPosition;
+    private String staffRole;
+    private String department;
+
+    public void setStaffName(){this.staffName = staffName;}
+    public String getStaffName(){return staffName;}
+
+    public void setStaffPosition(){this.staffPosition = staffPosition;}
+    public String getStaffPosition(){return staffPosition;}
+
+    public void setStaffRole(){this.staffRole = staffRole;}
+    public String getStaffRole(){return staffRole;}
+
+
 }
+
+
