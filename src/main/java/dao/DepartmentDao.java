e78d2e8c6347fdef4f654dee2dd7180dedc2cc73
[none] <[nwariara@safaricom.co.ke]>
2019-06-07 11:17:59 +0100

    added methods to department DAO.

--- src/main/java/dao/DepartmentDao.java
+++ src/main/java/dao/DepartmentDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.Department;
+import java.util.List;
+
 public interface DepartmentDao {
+
+    //List all departments
+    List<Department> getAll();
+
+    //Create a department
+    void add (Department department);
+
+    //Retrieve a single department
+    Department findById(int id);
+
+    // UPDATE
+    // void update(int id, String name);
+
+    // DELETE
+    // void deleteById(int id);
+    // void clearAllDepartments();
 }
