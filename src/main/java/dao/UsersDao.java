b78a365cc4bdbe8fa8788cb4c3f45aec332d24b0
[none] <[nwariara@safaricom.co.ke]>
2019-06-10 01:50:03 +0100

    added users Dao file.

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,5 +1,25 @@
 package dao;
 
+import models.Users;
+
+import java.util.List;
+
 public interface UsersDao {
+
+    //List all users
+    List<Users> getAll();
+
+    //Create a user
+    void add (Users users);
+
+    //Retrieve a single user
+    Users findById(int id);
+
+    // UPDATE
+    void update(int id, String name);
+
+    // DELETE
+    void deleteById(int id);
+    void clearAllUsers();
 
 }
