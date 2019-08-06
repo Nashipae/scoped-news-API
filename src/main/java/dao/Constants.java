515af851702c69c768c63bc68a0d143d13490035
Nashipae <nashkiki@gmail.com>
2019-08-06 18:02:41 +0300

    updated Constants class.

--- src/main/java/dao/Constants.java
+++ src/main/java/dao/Constants.java
@@ -1,7 +1,7 @@
 package dao;
 
 public class Constants {
-    public static final String USERNAME="postgres";
+    public static final String USERNAME="nwariara";
     public static final String PASSWORD="postgres";
 //    public static final String URL_TEST="postgres://localhost:5432/scoped_news_api_test";
     public static final String URL_PROD="postgres://localhost:5432/scoped_news_api";
